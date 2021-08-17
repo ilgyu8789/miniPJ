4806fdc3ef861e2ec6700eb1036333fd43767260
박일규 <t5nn123@naver.com>
2021-08-18 01:45:36 +0900

    미니프젝(전화번호부)

--- src/com/java/UserList.java
+++ src/com/java/UserList.java
@@ -7,15 +7,15 @@ import java.util.*;
 public class UserList {
 	static Scanner scan = new Scanner(System.in);
 	static File file = new File("./files/phonbook.txt");
-	
+
 	static ArrayList<HashMap<String, String>> pb_list = new ArrayList<HashMap<String, String>>();
-			
+
 	public static void PhoneNumBook() {
 		try {
 			Scanner scan = new Scanner(file);
-			
+
 			int idx = 1;
-			
+
 			while (scan.hasNextLine()) {
 
 				String[] split = scan.nextLine().split(", ");
@@ -25,12 +25,12 @@ public class UserList {
 				map.put("name", split[0]);
 				map.put("hp", split[1]);
 				map.put("tel", split[2]);
-				
+
 				pb_list.add(map);
 				idx++;
 			}
 			scan.close(); // try-catch를 쓸 때 scan은 try문 끝에서 닫아준다
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -49,7 +49,7 @@ public class UserList {
 	}
 
 	public static void Enrollment() {
-		
+
 		System.out.println("<2.등록>");
 		System.out.print(">이름:");
 		String name = scan.nextLine();
@@ -59,37 +59,42 @@ public class UserList {
 
 		System.out.print(">집전화:");
 		String tel = scan.nextLine();
-		
+
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("name", name);
 		map.put("hp", hp);
 		map.put("tel", tel);
-		
+
 		pb_list.add(map);
 	}
 
 	public static void Delete() {
 		System.out.println("<3.삭제>");
-		
+
 		System.out.print(">번호:");
 		int num = scan.nextInt();
-		
-		pb_list.remove(num -1);
+
+		pb_list.remove(num - 1);
 		System.out.println("[삭제되었습니다.]");
 	}
 
 	public static void Search() {
 		System.out.println("<4.검색>");
-		
+
 		System.out.print(">이름:");
 		String name = scan.nextLine();
-		
+
 		for (int i = 0; i < pb_list.size(); i++) {
-			
+			if (pb_list.get(i).get("name").contains(name)) {
+				// contains문의 값인 name은 어떤 이름을 입력하든
+				// 모든 값을 다 받는다 = 초기화라고 생각하면 편함
+				System.out.print(pb_list.get(i).get("name") + " ");
+				System.out.print(pb_list.get(i).get("hp") + " ");
+				System.out.println(pb_list.get(i).get("tel"));
+			}
+
 		}
-		
-		
-		
+
 	}
 
 	public static void End() {
